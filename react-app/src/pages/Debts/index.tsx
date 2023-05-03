e45934b8ab2275c52d31986757d30214ead78a1d
Luis Gustavo Zanetti <49000286+LuisGustavoCZP@users.noreply.github.com>
2023-05-02 22:45:51 -0300

    release/0.2.3

--- react-app/src/pages/Debts/index.tsx
+++ react-app/src/pages/Debts/index.tsx
@@ -1,11 +1,14 @@
-import { Fragment, ReactNode, useState } from 'react';
+import { FormEvent, Fragment, ReactNode, useState } from 'react';
 import { DebtList, NewDebtModal, NewPaymentModal } from '../../components';
 import type {UserProps, IDebt} from "../../models";
 import "./style.css";
 
+type IFilter = { [key : string] : any };
+
 export function Debts ({auth, debts} : UserProps)
 {
     const [modal, setModal] = useState<null | ReactNode>(null);
+    const [filter, setFilter] = useState<IFilter>({});
 
     function onClose ()
     {
@@ -28,23 +31,88 @@ export function Debts ({auth, debts} : UserProps)
 
     const NewDebtButton = (<button className='button' key={-1} onClick={openDebtModal}>Adicionar Dívida</button>)
 
+    function renderMonthFilter ()
+    {
+        
+    }
+
+    function currencyString (valor : number, currency = "BRL")
+    {
+        const cs = valor.toLocaleString("pt-br", { style: 'currency', currency});
+        return (
+            <span>
+                <span className="currency">{cs.slice(0, 2)}</span>
+                <span>{cs.slice(3)}</span>
+            </span>
+        );
+    }
+
+    function renderUserFilter ()
+    {
+        const filterHandler = (e : FormEvent<HTMLSelectElement>) => 
+        {
+            const select = e.target as HTMLSelectElement;
+            const newFilter = Object.assign({}, filter);
+
+            if(select.value === "any") delete newFilter["creditor"];
+            else newFilter["creditor"] = select.value;
+
+            setFilter(newFilter);
+        }
+
+        const creditorSet = new Set();
+        const creditors = debts.list.filter(debt => 
+        {
+            if(creditorSet.has(debt.creditor)) return false;
+            creditorSet.add(debt.creditor);
+            return true;
+        }).map((debt, i) => <option key={i} value={debt.creditor}>{debt.creditor}</option>)
+
+        creditors.unshift(<option key={-1} value="any" defaultChecked>Todos</option>)
+
+        return (
+            <select onChange={filterHandler}>
+                {creditors}
+            </select>
+        );
+    }
+
     function renderDebts (list: IDebt[])
     {
+        const totalDebts = list.reduce((total, debt) => total += debt.installment.cost, 0);
+
+        const totalString = filter["creditor"] ? ` com ${filter["creditor"]} ` : " total "
+
         return (
             <Fragment>
                 <span>
-                    <h2>Suas <b>dívidas</b>, {auth.name}</h2>
-                    { debts && NewDebtButton }
+                    <h2>Suas <b>dívidas</b>, {auth.name}. Sua divida {totalString} é: <span className='red'>{currencyString(totalDebts)}</span></h2>
+                    <span>
+                        { renderUserFilter () }
+                        { debts && NewDebtButton }
+                    </span>
                 </span>
-                <DebtList debts={list} pay={openPaymentModal}/>
+                <div className='container'>
+                    <DebtList debts={list} pay={openPaymentModal}/>
+                </div>
             </Fragment>
         )
     }
 
+    function filterList (list: IDebt[])
+    {
+        const entries = Object.entries(filter);
+
+        if(entries.length === 0) return list;
+        return list.filter((debt : any) => entries.some(([key, value]) => debt[key] && debt[key] === value ));
+    }
+
+    const list = filterList(debts.list);
+
     return (
         <Fragment>
             <main className='Debts'>
-                {renderDebts(debts.list)}
+                {renderDebts(list)}
             </main>
             {modal}
         </Fragment>
